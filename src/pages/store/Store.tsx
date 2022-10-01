c37295321ee2a4cd197895791f743e54549eb3a7
UniM0cha <solst_ice@naver.com>
2022-10-01 13:22:07 +0900

    1001 - 정보탭 구현

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -5,6 +5,32 @@ import Header from '../../components/Header';
 import Seperator from '../../components/Seperator';
 import MenuItem from './MenuItem';
 import styles from './Store.module.scss';
+import {
+  IoCallOutline,
+  IoInformationCircleOutline,
+  IoLocationOutline,
+  IoNavigateOutline,
+} from 'react-icons/io5';
+import classNames from 'classnames';
+
+const tempData = {
+  call: '010-2474-6837',
+  location: '서울 서초구 반포대로 24길 20 1층',
+  wayto: `서초역 1번 출구(교대역 방면)에서 바로 우회전 후
+현대 자동차 정비소 끼고 좌회전 하시면
+바로 있습니다.`,
+  information: `안녕하세요.
+스타일카케어 서초직영점입니다
+
+스타일카케어는 강남논현본점을 시작으로 강남역삼점 강 남대치점 서울신윌점 그리고 이번 강남 서초직영점까지 5개의 매장이 함께하고 있는 중견업체입니다.
+또한 2021 대한민국 인기브랜드 대상을 수상하며 고객 뿐 아니라 업계에서도 인정받는다는 것을 증명하였습니다.
+
+저희 스타일카케어는 디테일링, 손세차, 유리막코팅, PPF, 사고수리, 썬팅, 카오 디오, 방음 등 차량 관리 토탈 케어샵으로 확실한 시공과 사후 처리까지 완벽하게 진행해드리고 있으며,
+전 직원들의 철저한 교육을 통해 꼼꼼하고 확실한 작업을 도와드리고 있습니다.
+
+차량에 관련한 모든 부분 저희 스타일카케어를 통해 진행하신다면 후회 없으실거라 자신합니다.
+자세한 문의사항은 010.4465.6111 으로 문의주시면 친절히 답변해드리겠습니다.`,
+};
 
 function Store() {
   const { slug } = useParams<{ slug: string }>();
@@ -66,17 +92,76 @@ function Store() {
         </Tab>
 
         <Tab eventKey="info" title="정보" className={styles.tab}>
-          asdfsda
+          <InfoItem type="call" data={tempData.call} />
+          <Seperator />
+          <InfoItem type="location" data={tempData.location} />
+          <Seperator />
+          <InfoItem type="wayto" data={tempData.wayto} />
+          <Seperator />
+          <InfoItem type="information" data={tempData.information} />
         </Tab>
-        <Tab
+        {/* <Tab
           eventKey="review"
           title="리뷰"
           className={styles.tab}
           disabled
-        ></Tab>
+        ></Tab> */}
       </Tabs>
     </>
   );
+}
+
+interface InfoItemProps {
+  type: string;
+  data: string;
+}
+
+function InfoItem({ type, data }: InfoItemProps) {
+  switch (type) {
+    case 'call':
+      return (
+        <div className={styles.info_flex}>
+          <div className={styles.icon}>
+            <IoCallOutline className={styles.icon} size={20} color="gray" />
+          </div>
+          <div className={styles.info_content}>{data}</div>
+        </div>
+      );
+    case 'location':
+      return (
+        <div className={styles.info_flex}>
+          <div className={styles.icon}>
+            <IoLocationOutline className={styles.icon} size={20} color="gray" />
+          </div>
+          <div className={styles.info_content}>{data}</div>
+        </div>
+      );
+    case 'wayto':
+      return (
+        <div className={styles.info_flex}>
+          <div className={styles.icon}>
+            <IoNavigateOutline size={20} color="gray" />
+          </div>
+          <div className={styles.info_content}>{data}</div>
+        </div>
+      );
+    case 'information':
+      return (
+        <div className={styles.info_flex}>
+          <div className={styles.icon}>
+            <IoInformationCircleOutline size={20} color="gray" />
+          </div>
+          <div className={styles.info_content}>{data}</div>
+        </div>
+      );
+    default:
+      return (
+        <>
+          {type}
+          {data}
+        </>
+      );
+  }
 }
 
 export default Store;
